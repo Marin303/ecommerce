0ae226667f85584eb6eb8351e0c50c5ce7cd9062
Marin <marin.muktic3@gmail.com>
2023-05-30 19:33:37 +0200

    new method, new components inside product.js

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,8 @@
-import { model, Schema } from "mongoose";
+import { model, Schema, models } from "mongoose";
 const ProductSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
 });
 
-export const Product = model("Product", ProductSchema);
+export const Product = models.Product || model("Product", ProductSchema);
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,25 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
+import { Router, useRouter } from "next/router";
 import { useState } from "react";
 
 export default function NewProduct() {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [price, setPrice] = useState('')
+    const [goToProduct, setGoToProduct] = useState(false)
+
+    const router = useRouter()
 
     async function createProduct(e){
         e.preventDefault()
         const data = {title, description,price}
         await axios.post('/api/products', data)
+        setGoToProduct(true)
+    }
+    if(goToProduct){
+      router.push('/products')
     }
   return (
     <Layout>
