be0c5f83a761bde0bbd7e7b54b039804d2924801
Marin <marin.muktic3@gmail.com>
2023-06-28 16:57:34 +0200

    showing catergory names, axios get

--- pages/categories.js
+++ pages/categories.js
@@ -1,30 +1,51 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Categories() {
   const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }, []);
   async function saveCategory(ev) {
-    ev.preventDefault()
-   await axios.post('/api/categories', {name})
-   setName('')
+    ev.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
   }
   return (
     <Layout>
       <h1>Categories</h1>
       <label>New Category name</label>
       <form className="flex gap-1" onSubmit={saveCategory}>
-        <input 
-        type="text" 
-        placeholder={"Category name"} 
-        className="mb-0"
-        value={name}
-        onChange={ev => setName(ev.target.value)}
-         />
+        <input
+          type="text"
+          placeholder={"Category name"}
+          className="mb-0"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+        />
         <button className="btn-primary py-1" type="submit">
           Save
         </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr>
+                <td>{category.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
